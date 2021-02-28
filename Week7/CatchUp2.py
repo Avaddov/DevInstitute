fedb3c17a16800edbe3170a80abb6e4b4e310766
FiendRiver <fiendriver@gmail.com>
2021-02-28 09:55:08 +0200

    beginningofweek8

---
+++ Week7/CatchUp2.py
@@ -0,0 +1,54 @@
+# create a function that takes a list of numbers as a parameter and prints the sum of the all the numbers in the list
+# def sum_list(nums):
+#     sum = 0
+#     for num in nums:
+#         sum += num
+#     print(sum)
+
+
+### Make a list of magician’s names.
+## Pass the list to a function called show_magicians(), which prints the name of each magician in the list.
+# magicians = ["Harry", "David", "Criss"]
+# def show_magicians(magician_list):
+#     for name in magician_list:
+#         print(name)
+# show_magicians(magicians)
+# # # Write a function called make_great() that modifies the list of magicians by adding the phrase "the Great" to each magician’s name.
+# # # Call show_magicians() to see that the list has actually been modified.
+# def make_great(magician_list):
+#     for index, name in enumerate(magician_list):
+#         magician_list[index] = name + " The Great"
+# make_great(magicians)
+# show_magicians(magicians)
+
+#OR
+
+# magicians = ['Houdini', 'Copperfield', 'Blaine']
+
+# def show_magicians():
+#     for name in magicians:
+#         print(name)
+
+# def make_great():
+#     great = []
+#     for name in magicians:
+#         great.append(f'The great {name}')
+#     print(great)
+
+
+# create a function called shopping list that asks the user for items untill the user decides to exit then prints out the list
+# def shopping_list():
+#     itemlist = []
+#     active = True
+#     while active:
+#         item = input("What would you like to buy? Type 'exit' to stop.\n")
+#         if user != 'exit':
+#             itemlist.append(item)
+#         else:
+#             active = False
+#     print(itemlist)
+
+# shopping_list()
+
+
+
